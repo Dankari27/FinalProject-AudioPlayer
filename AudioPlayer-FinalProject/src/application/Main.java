11d1a0883269296afa63a9c33ad5d4d1108888fb
Dankari27 <richard.savoca12@gmail.com>
2023-12-09 12:53:55 -0500

    added snap feature to the window. this allows the window to be easily placed to the edges of the screen if desired.

--- AudioPlayer-FinalProject/src/application/Main.java
+++ AudioPlayer-FinalProject/src/application/Main.java
@@ -3,79 +3,101 @@ import javafx.scene.input.MouseEvent;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 
-// The main class that extends Application, required for JavaFX applications
 public class Main extends Application {
-	   private double xOffset = 0;
-	    private double yOffset = 0;
-	// The start method is the entry point for JavaFX applications
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			
-			// Load the FXML file to create the UI layout
-			AnchorPane root = FXMLLoader.load(getClass().getResource("/Home.fxml"));
-			
-			//make undecorated window movable
-	        root.setOnMousePressed((EventHandler<? super MouseEvent>) new EventHandler<MouseEvent>() {
-	            @Override
-	            public void handle(MouseEvent event) {
-	                xOffset = event.getSceneX();
-	                yOffset = event.getSceneY();
-	            }
-	        });
-	        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
-	            @Override
-	            public void handle(MouseEvent event) {
-	                primaryStage.setX(event.getScreenX() - xOffset);
-	                primaryStage.setY(event.getScreenY() - yOffset);
-	            }
-	        });
-	        
-	        
-			
-			// Create a scene with the UI layout and set its dimensions (width: 600, height: 400)
-			Scene scene = new Scene(root);
-			
-			//sets system icon
-			Image icon = new Image("icon.png");
-			primaryStage.getIcons().add(icon);
-			
-			// Set the scene to the primary stage (the main window)
-			primaryStage.setScene(scene);
-			
-		
-			
-			// Make the window not resizable
-			primaryStage.setResizable(false);
-			
-			//set opacity
-			primaryStage.setOpacity(0.76);
-			
-			primaryStage.initStyle(StageStyle.TRANSPARENT);
-			
-		
-			//remove window handle bars
-			primaryStage.initStyle(StageStyle.UNDECORATED);
-		
-			// Show the primary stage with the loaded scene
-			primaryStage.show();
-	
-			
-		} catch (Exception e) {
-			// Print the stack trace in case of an exception
-			e.printStackTrace();
-		}
-	}
+    private double xOffset = 0;
+    private double yOffset = 0;
 
-	
-	// The main method, which launches the JavaFX application
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            AnchorPane root = FXMLLoader.load(getClass().getResource("/Home.fxml"));
+
+            root.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    xOffset = event.getSceneX();
+                    yOffset = event.getSceneY();
+                }
+            });
+            
+            //allows undecorated window to be dragged
+            root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    primaryStage.setX(event.getScreenX() - xOffset);
+                    primaryStage.setY(event.getScreenY() - yOffset);
+
+                    //snap to edge when dragging
+                    snapToEdge(primaryStage);
+                }
+            });
+
+            Scene scene = new Scene(root);
+            
+            //adds system icon
+            Image icon = new Image("icon.png");
+            primaryStage.getIcons().add(icon);
+            
+            primaryStage.setScene(scene);
+            
+            //makes window unable to be resized
+            primaryStage.setResizable(false);
+            //adds transparency
+            primaryStage.setOpacity(0.76);
+            primaryStage.initStyle(StageStyle.TRANSPARENT);
+            
+            //removes window border
+            primaryStage.initStyle(StageStyle.UNDECORATED);
+
+            primaryStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    //method to handle snapping feature
+    private void snapToEdge(Stage stage) {
+        Screen screen = Screen.getPrimary();
+        double screenWidth = screen.getBounds().getWidth();
+        double screenHeight = screen.getBounds().getHeight();
+
+        double windowWidth = stage.getWidth();
+        double windowHeight = stage.getHeight();
+
+        //define the snapping margin (e.g., 10 pixels)
+        //i.e how far away the window must be from the edge of the screen in order to snap to it
+        double snapMargin = 40;
+
+        //check if the window is close to the left edge
+        if (stage.getX() < snapMargin) {
+            stage.setX(0);
+        }
+
+        //check if the window is close to the right edge
+        if (stage.getX() + windowWidth > screenWidth - snapMargin) {
+            stage.setX(screenWidth - windowWidth);
+        }
+
+        //check if the window is close to the top edge
+        if (stage.getY() < snapMargin) {
+            stage.setY(0);
+        }
+
+        //check if the window is close to the bottom edge
+        if (stage.getY() + windowHeight > screenHeight - snapMargin) {
+            stage.setY(screenHeight - windowHeight);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
